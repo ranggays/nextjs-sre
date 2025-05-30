30210ffa59acc4cbf010c2e428fad6f49277f741
ranggays <ranggayoviesaputra@gmail.com>
2025-05-30 22:06:40 +0700

    add edges api

--- src/app/api/edges/route.ts
+++ src/app/api/edges/route.ts
@@ -2,28 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(req: NextRequest){
-    const articleIdParam = req.nextUrl.searchParams.get('articleId');
-
-    if (!articleIdParam) {
-        return NextResponse.json({ error : 'Missing articleId' }, { status: 400});
-    };
-
-    const articleId = parseInt(articleIdParam, 10);
-
-    if (isNaN(articleId)){
-        return NextResponse.json({ error: 'Invalid articleId'}, { status: 400});
-    };
-
     try {
-        const edges = await prisma.edge.findMany({
-            where: {
-                articleId,
-            }
-        });
-
+        const edges = await prisma.edge.findMany();
         return NextResponse.json(edges);
     } catch (error) {
-        console.error("Error fetching nodes: ", error);
+        console.error("Error fetching edges: ", error);
         return NextResponse.json({error : 'Failed to fetch node'}, {status: 500});
     };
 }
