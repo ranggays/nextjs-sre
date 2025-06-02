4c7daa774c442c27a42909ec876a3c445ec5f898
ranggays <ranggayoviesaputra@gmail.com>
2025-06-02 16:38:41 +0700

    edit for acces node atribute

--- src/components/EdgeDetail.tsx
+++ src/components/EdgeDetail.tsx
@@ -2,13 +2,48 @@
 
 import { Modal, Table, Text } from '@mantine/core';
 import { ExtendedEdge } from '../types';
+import { useEffect, useState } from 'react';
 
 interface EdgeDetailProps {
   edge: ExtendedEdge | null;
   onClose: () => void;
-}
+};
+
+interface PopulatedEdge {
+  id: number;
+  label: string | null;
+  relation: string | null;
+  from: {
+    id: number;
+    title: string;
+  };
+  to: {
+    id: number;
+    title: string;
+  };
+};
 
 export default function EdgeDetail({ edge, onClose }: EdgeDetailProps) {
+
+  const [edgeNode, setEdgeNode] = useState<PopulatedEdge>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (edge?.id){
+      setLoading(true);
+      fetch(`/api/edges/${edge?.id}`)
+        .then((res) => {
+          if (!res.ok){
+            throw new Error(`Failed to fetch: ${res.status}`);
+          }
+          return res.json();
+        })
+        .then((data) => setEdgeNode(data))
+        .catch((err) => console.error('failed to fetch edge details:', err))
+        .finally(() => setLoading(false));
+    }
+  }, [edge?.id]);
+
   if (!edge) return null;
 
   return (
@@ -16,8 +51,8 @@ export default function EdgeDetail({ edge, onClose }: EdgeDetailProps) {
       <Table striped highlightOnHover withTableBorder>
         <tbody>
           <tr><td>Jenis Relasi</td><td>{edge.relation || '-'}</td></tr>
-          <tr><td>Dari Artikel</td><td>{edge.from}</td></tr>
-          <tr><td>Ke Artikel</td><td>{edge.to}</td></tr>
+          <tr><td>Dari Artikel</td><td>{edgeNode?.from?.title}</td></tr>
+          <tr><td>Ke Artikel</td><td>{edgeNode?.to?.title}</td></tr>
         </tbody>
       </Table>
 
