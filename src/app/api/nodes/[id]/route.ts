f26e646866edba16a03843b597b8ffcfae4fc025
ranggays <ranggayoviesaputra@gmail.com>
2025-06-06 19:09:47 +0700

    make api route for nodes id

---
+++ src/app/api/nodes/[id]/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(req: NextRequest, {params} : {
+    params: Promise <{id: string}>
+}){
+    const { id: idParam } = await params;
+
+    const id = Number(idParam);
+
+    return NextResponse.json({msg : 'Delete Node Succed', id: id}, {status: 201});
+}
